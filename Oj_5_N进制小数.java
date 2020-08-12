69ff355cb3dc6cc34911ac20fdd5946cc151fc83
Zpyyy <60057958+Zpyyy@users.noreply.github.com>
2020-08-12 17:12:25 +0800

    Create Oj_5_N进制小数.java

---
+++ Oj_5_N进制小数.java
@@ -0,0 +1,28 @@
+package demo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * 5. N进制小数
+ * 题目描述：编写程序实现将任意10进制正小数m转换成n进制的正小数，小数点后保留10位小数。
+ * 输入：输入包含两个数m,n，用空格隔开。输入包含多组测试，当m,n都为0时输入结束。
+ * 输出：输出10进制正小数m的n进制小数。结果保留10位小数。
+ *
+ * Created by zWX945272 on 2020/8/12.
+ */
+
+public class OJ_5 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextInt()){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a+b);
+        }
+    }
+
+}
+
+
